543ba5366f7b770b03392fec21a96783b128a2b8
Sebastian Szymanek <sebaa75@gmail.com>
2018-03-15 20:57:57 +0100

    Add project files.

---
+++ GeoLib.Tests/GeoLibServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using GeoLib.Contracts;
+using GeoLib.Data.Entities;
+using GeoLib.Data.RepositoryInterfaces;
+using GeoLib.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class GeoLibServiceTest
+    {
+        [TestMethod]
+        public void Test_ServiceOperation_GetZipInfo()
+        {
+            //Arrange - zaranzuj sztuczne repozytorium, ktore zwraca sztuczne dane
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            const string ZIP = "80200";
+
+
+
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "Gdansk",
+                State = new State() { Abbreviation = "PR" },
+                Zip = "80200"
+            };
+
+            mockZipCodeRepository.Setup(mr => mr.GetByZip(ZIP)).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object, null);
+            geoService.GetZipInfo("80200");
+
+
+            //Act - zadzialaj i wykonaj ta metode
+            var returnedZipCode = geoService.GetZipInfo(ZIP);
+
+            //Assert - upewnij sie, ze dziala poprawnie
+            Assert.IsTrue(returnedZipCode.City == "Gdansk" , "ZipCode should be Gdansk");
+            Assert.IsTrue(returnedZipCode.State == "PR");
+        }
+    }
+}
---
+++ GeoLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeoLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeoLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("466de52b-c118-41e3-95bc-9a574ea65d1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
